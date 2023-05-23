8ef7cec36bfa0b08ddd4c955a7ef2197483e8446
Michael Soebroto <msoebrot@ucsc.edu>
2023-05-22 18:28:16 -0700

    Update server script.js

--- server/script.js
+++ server/script.js
@@ -1,1 +1,34 @@
 //WEBSITE SERVER
+
+const express = require('express');
+const request = require("request");
+
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+//Variable where we save the deckid for the current game
+deckid = 0;
+
+//Function to start a game
+app.get("/start/:username", (req, res) => {
+    let username = req.params['username'];
+    
+
+});
+
+//Function to hit
+app.get("/hit/:username", (req, res) => {
+
+});
+
+//Function to stay
+app.get("/stay/:username", (req, res) => {
+
+});
+
+//Function to update balance
+app.get("/balance/:username", (req, res) => {
+
+});
