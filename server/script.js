3a1d64305454f47fcdb38901e06b8b32b98b9ffd
Michael Soebroto <msoebrot@ucsc.edu>
2023-05-23 13:03:56 -0700

    Update script.js
    Add functionality for getting a deck and drawing a card.

--- server/script.js
+++ server/script.js
@@ -9,39 +9,116 @@ const cors = require('cors');
 app.use(cors());
 
 //Variable where we save the deckid for the current game
-deckid = 0;
+let deckid = '';
+let remaining_cards = -1;
+let dealer_total = -1;
+let player_total = -1;
 
 //Function to start a game
 app.get("/start/:username", (req, res) => {
     let username = req.params['username'];
     console.log('start');
     console.log(username);
+    dealer_total = 0;
+    player_total = 0;
+    
+    if(remaining_cards < 60) {
+        //create a deck
+        let deckurl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6';
+        request(deckurl, (error, response, body)=>{
+            // Printing the error if occurred
+		    if(error) console.log(error)
+	   
+		    // Printing status code
+		    console.log(response.statusCode);
 
-    //create a deck
-    let deckurl = 'https://deckofcardsapi.com/api/deck/new/';
-    //draw a card
-    let cardurl = 'https://deckofcardsapi.com/api/deck/=${deckid}/draw/?count=2';
+            console.log(body);
+
+            body = JSON.parse(body);
+            deckid = body.deck_id;
+            remaining_cards = body.remaining;
+
+            console.log(deckid);
+            console.log(remaining_cards);
+
+            let starturl = `https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=2`;
+            console.log(starturl)
+            request(starturl, (error, response, body)=>{
+                if(error) console.log(error)
+	   
+		        // Printing status code
+		        console.log(response.statusCode);
+
+                console.log(body);
+
+                body = JSON.parse(body);
+                deckid = body.deck_id;
+                remaining_cards = body.remaining;
+                console.log(deckid);
+                console.log(remaining_cards);
+            });
+        });
+    }
+    else {
+        let starturl = `https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=2`;
+        console.log(starturl)
+        request(starturl, (error, response, body)=>{
+            if(error) console.log(error)
+	   
+		    // Printing status code
+		    console.log(response.statusCode);
+
+            console.log(body);
+
+            body = JSON.parse(body);
+            deckid = body.deck_id;
+            remaining_cards = body.remaining;
+            console.log(deckid);
+            console.log(remaining_cards);
+        });
+    }
+    res.send('hello');
 });
 
 //Function to hit
 app.get("/hit/:username", (req, res) => {
-    let hit = req.params['username'];
+    let username = req.params['username'];
     console.log('hit');
     console.log(username);
     //draw a card
+    let hiturl = `https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=2`;
+    console.log(hiturl)
+    request(hiturl, (error, response, body)=>{
+        if(error) console.log(error)
+	   
+		// Printing status code
+		console.log(response.statusCode);
+
+        console.log(body);
+
+        body = JSON.parse(body);
+        deckid = body.deck_id;
+        remaining_cards = body.remaining;
+        console.log(deckid);
+        console.log(remaining_cards);
+    });
 
 });
 
 //Function to stay
-app.get("/stay/:username", (req, res) => {
-    let stay = req.params['username'];
-    console.log('stay');
+app.get("/stand/:username", (req, res) => {
+    let username = req.params['username'];
+    console.log('stand');
     console.log(username);
 });
 
 //Function to update balance
 app.get("/balance/:username", (req, res) => {
-    let balance = req.params['username'];
+    let username = req.params['username'];
     console.log('balance');
     console.log(username);
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!'); //Go to http://localhost:3000/');
 });
--- server/test-client.js
+++ server/test-client.js
@@ -1,6 +1,21 @@
 //Temporary test for server functionality
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let username = "bill";
+
 let xhr = new XMLHttpRequest(); //Defines xmlhttp objject
 xhr.open("GET", `http://localhost:3000/start/${username}`); // request to the website
 xhr.send(); //sends the request
+
+/*
+let xhr2 = new XMLHttpRequest(); //Defines xmlhttp objject
+xhr2.open("GET", `http://localhost:3000/hit/${username}`); // request to the website
+xhr2.send(); //sends the request
+
+let xhr3 = new XMLHttpRequest(); //Defines xmlhttp objject
+xhr3.open("GET", `http://localhost:3000/stand/${username}`); // request to the website
+xhr3.send(); //sends the request
+
+let xhr4 = new XMLHttpRequest(); //Defines xmlhttp objject
+xhr4.open("GET", `http://localhost:3000/balance/${username}`); // request to the website
+xhr4.send(); //sends the request
+*/
