12c93a55ff0033e928d5bd0d7e71f55b1045cb6c
Michael Soebroto <msoebrot@ucsc.edu>
2023-06-08 09:04:09 -0700

    Might work idk

--- game.js
+++ game.js
@@ -19,18 +19,23 @@ xhr_user.onload = function() {
 
 
 function getBet() {
-    console.log("getBet");
-    validBet = false;
-    if(started == false) {
-        bet = Number(pot.value);
-        if(bet > bal || bet <= 0)
+    console.log("getBet"); 
+    let pool = Number(pot.value);
+    console.log(pool)
+    if(started === false && pool > 0 || pool === bet) {
+        if(pool > bal || pool <= 0)
         {
             alert('Invalid bet');
         }
         else {
             validBet = true;
+            bet = pool;
         }
     }
+    else {
+        validBet = false;
+    }
+    console.log(started)
 }
 
 function returnToLogin() {
@@ -47,15 +52,18 @@ document.getElementById("buttonstop").addEventListener("click", stand);
 // Function to handle the "Deal" button click
 function deal() {
     getBet()
+    console.log(validBet);
+    console.log(started);
     if(validBet == false || started == true) {
         return;
     }
+    console.log('in');
+    started = true;
     // TODO: Implement the logic for dealing the cards
     let xhr_start = new XMLHttpRequest(); //Defines xmlhttp objject
     xhr_start.open("GET", `http://localhost:3000/start/${user}/${bet}`); // request to the website
     xhr_start.send(); //sends the request
     xhr_start.onload = function() {
-        started = true;
         let startbody = JSON.parse(xhr_start.responseText);
         //console.log(startbody);
         
@@ -81,9 +89,12 @@ function deal() {
 
     // Function to handle the "Hit" button click
 function hit() {
+    console.log(validBet);
+    console.log(started);
     if(validBet == false || started == false) {
         return;
     }
+    console.log('in')
     // TODO: Implement the logic for hitting (drawing) a card
     let xhr_hit = new XMLHttpRequest(); //Defines xmlhttp objject
     xhr_hit.open("GET", `http://localhost:3000/hit/${user}`); // request to the website
@@ -117,9 +128,12 @@ function hit() {
 
     // Function to handle the "Stand" button click
 function stand() {
+    console.log(validBet);
+    console.log(started);
     if(validBet == false || started == false) {
         return;
     }
+    console.log('in');
     // TODO: Implement the logic for standing (ending the player's turn)
     let xhr_stand = new XMLHttpRequest(); //Defines xmlhttp objject
     xhr_stand.open("GET", `http://localhost:3000/stand/${user}`); // request to the website
